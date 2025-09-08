2162398064267054656531d3ae0412abbba73f09
Andy Hui <chhuiwork@gmail.com>
2025-09-08 18:35:42 +0800

    Feat: Implement functionality for standard parking boy to park cars

--- src/main/java/com/afs/parkinglot/StandardParkingBoy.java
+++ src/main/java/com/afs/parkinglot/StandardParkingBoy.java
@@ -6,4 +6,8 @@ public class StandardParkingBoy {
     StandardParkingBoy() {
         managedParkingLot = new ParkingLot();
     }
+
+    public ParkingTicket park(Car car){
+        return managedParkingLot.park(car);
+    }
 }
